6c10452d438869606e06824906081085f9915035
Grant Johnson <grant@amadensor.com>
2022-01-06 21:58:15 -0700

    Bug fixes and convert loader to pokes instead of address specific load

--- build_d64.sh
+++ build_d64.sh
@@ -1,9 +1,11 @@
 petcat -w2 -o label.prg -- label.bas
+petcat -w2 -o load.prg -- load.bas
 petcat -text -w2 -o ascii_convert.pet ascii_convert.txt
 petcat -text -w2 -o reader3.pet reader3.txt
 petcat -text -w2 -o ml_loader.pet ml_loader.txt
 c1541 -format test,t1 d64 test.d64
-c1541 -attach test.d64 -write label.prg 
+c1541 -attach test.d64 -write label.prg label
+c1541 -attach test.d64 -write load.prg load
 c1541 -attach test.d64 -write reader3.pet reader3,s
 c1541 -attach test.d64 -write ascii_convert.pet ascii-convert,s
 c1541 -attach test.d64 -write ml_loader.pet ml-loader,s
